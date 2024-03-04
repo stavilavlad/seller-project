0e33672083b675b9752a338cfefefb97104afdf6
Vlad <vladstavila@gmail.com>
2024-03-04 22:21:57 +0200

    fixed bug

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const serverURL = "hhtps://server-self-ten-30.vercel.app";
+const serverURL = "https://server-self-ten-30.vercel.app";
 
 export const customFetch = axios.create({
   baseURL: serverURL,
